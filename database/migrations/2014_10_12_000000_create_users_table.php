92f63953950100d97af9a8409b22a844a9910835
Marcellinom <marcellino.janitra@yahoo.com>
2022-10-17 23:15:50 +0700

    user Model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->boolean('soft_deleted')->default(false);
+            $table->integer('created_at')->useCurrent();
         });
     }
 
