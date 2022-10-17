eabc098f771c2ce2ea47b1cb4570caec038686b6
Marcellinom <marcellino.janitra@yahoo.com>
2022-10-17 23:16:17 +0700

    TOOL buat auto generate htdocs getter tiap model

---
+++ app/Console/Commands/GenerateModelGetter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Barryvdh\Reflection\DocBlock;
+use Barryvdh\Reflection\DocBlock\Serializer as DocBlockSerializer;
+use File;
+use Illuminate\Console\Command;
+use ReflectionClass;
+use function app_path;
+use function explode;
+use function preg_replace;
+use function scandir;
+use function str_replace;
+use function strtolower;
+use function substr;
+
+class GenerateModelGetter extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:model-getter';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'generate model getter by column name and type';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     * @throws \ReflectionException
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function handle()
+    {
+        // loop semua model dalam App/Models
+        foreach (scandir(app_path('Models')) as $class) {
+            if ($class === '.' || $class === '..') continue;
+
+            $reflection_class = new ReflectionClass($class_name = substr('App\\Models\\'.$class, 0, -4));
+            $php_docs = new DocBlock($reflection_class, new DocBlock\Context($reflection_class->getNamespaceName()));
+
+            // loop untuk menghapus getter lama
+            foreach ($php_docs->getTags() as $tag) {
+                if ($tag instanceof DocBlock\Tag\MethodTag) {
+                    // getMethodName jadi get_method_name
+                    $method_name = strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $tag->getMethodName()));
+                    $action = explode('_', $method_name, 2)[0];
+
+                    if ($action === 'get' or $action === 'is') $php_docs->deleteTag($tag);
+                }
+            }
+            // loop properties untuk menambah getter baru
+            foreach ($class_name::ATTRIBUTES as $atr => $type) {
+                $cammelCaseMethod = str_replace('_', '', ucwords($atr, '_'));
+                $method = $type === 'bool' ? "is$cammelCaseMethod" : "get$cammelCaseMethod";
+                $php_docs->appendTag(DocBlock\Tag::createInstance("@method $type $method()", $php_docs));
+            }
+            // nulis docs barunya
+            $content = File::get($reflection_class->getFileName());
+            $serializer = new DocBlockSerializer();
+            $new_doc = $serializer->getDocComment($php_docs);
+            $content = str_replace($reflection_class->getDocComment(), $new_doc, $content);
+            File::put($reflection_class->getFileName(), $content);
+        }
+        return Command::SUCCESS;
+    }
+}
