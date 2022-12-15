347ca42826bac80445531eb886dadfca39571ca6
akunlainfiqi <fiqi296@gmail.com>
2022-12-16 03:39:18 +0700

    enable cors poll

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,11 +30,11 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware([EnableCors::class, 'api'])
+            Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware([EnableCors::class, 'web'])
+            Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
     }
