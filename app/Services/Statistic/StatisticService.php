05ce9b974905019d80d3540ce2979e2deb40b82a
Marcellinom <marcellino.janitra@yahoo.com>
2022-12-16 08:26:11 +0700

    statistic item history

--- app/Services/Statistic/StatisticService.php
+++ app/Services/Statistic/StatisticService.php
@@ -5,19 +5,33 @@ namespace App\Services\Statistic;
 use App\Models\Shared\JsonSerialize;
 use App\Models\Transaction;
 use App\Models\User;
-
 class StatisticService
 {
     public function execute(User $user): JsonSerialize
     {
-        $marketplace_ids = [];
+        $marketplace_ids = [];;
         foreach ($user->shop() as $shop) {
             foreach ($shop->marketplace() as $marketplace) {
                 $marketplace_ids[] = $marketplace->getId();
             }
         }
+        $transaction = Transaction::whereIn('marketplace_id', $marketplace_ids);
+        $income = $transaction->sum('total_price');
+        $items_history = [];
+        $i = 0;
+        foreach ($transaction->get() as $transaction) {
+            /** @var Transaction $transaction */
+            foreach ($transaction->getItems() as $item) {
+                $product = $item->product();
+                $items_history[$i]['product_name'] = $product->getName();
+                $items_history[$i]['quantity'] = $item->getQuantity();
+                $items_history[$i]['transaction_at'] = $transaction->getCreatedAt();
+                if ($i++>=10) break;
+            }
+        }
         return JsonSerialize::make([
-            'income' => Transaction::whereIn('marketplace_id', $marketplace_ids)->sum('total_price')
+            'income' => $income,
+            'transaction_item_history' =>$items_history
         ]);
     }
 }
